5463647b52746689bd9f6d465ddf33947d083100
Amanda Zhou <aezhou@mit.edu>
2015-11-10 14:20:28 -0500

    basic location tracking up
    using significant change location tracking atm

--- MigraineAid/ViewController.swift
+++ MigraineAid/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSLog("hi")
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    var locations = [CLLocation]()
+    
+    lazy var locationManager: CLLocationManager! = {
+        let manager = CLLocationManager()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestAlwaysAuthorization()
+        return manager
+    }()
+    
+    override func viewWillAppear(animated: Bool) {
+        NSLog("about to appear")
+        if CLLocationManager.locationServicesEnabled() {
+            NSLog("location services on")
+            locationManager.startMonitoringSignificantLocationChanges()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.locations.appendContentsOf(locations)
+        for loc in locations {
+            NSLog("New location is %@", loc)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        NSLog("Location manager failed with error: %@", error)
+        if error.domain == kCLErrorDomain && CLError(rawValue: error.code) == CLError.Denied {
+            //user denied location services so stop updating manager
+            manager.stopUpdatingLocation()
+        }
+    }
+    
 }
+
 
