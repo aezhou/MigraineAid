2feb162f265e05c8c963f8e42ead25a9c09b4ee2
Amanda Zhou <aezhou@mit.edu>
2015-11-10 14:08:26 -0500

    Initial Commit

---
+++ MigraineAid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MigraineAid
+//
+//  Created by Amanda Zhou on 11/10/15.
+//  Copyright © 2015 Amanda Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
