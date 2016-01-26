056f496a0421e479349f1568852325ea39eacec2
Amanda Zhou <aezhou@mit.edu>
2016-01-25 22:23:33 -0400

    basic framework for script

---
+++ scripts/weather-fetching/script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+var Parse = require('parse/node');
+
+Parse.initialize("RPEelxnIRofOu7fHaBBV1OjZNPESu9cjWIvu2NeB", "v4trnTaenDyf6ImpbxWIE6JGmU60OYRAcxasuvxF");
+
+console.log('test');	
